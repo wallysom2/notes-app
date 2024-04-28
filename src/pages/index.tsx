fd804e843c786b1f0e2c13f61bc45d1f0366229d
matheus <wallysom2@gmail.com>
2024-04-28 10:56:54 -0300

    :bricks: ci: adding prisma schemas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,6 @@ import NotesPage from "~/components/Notes";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -19,16 +17,13 @@ export default function Home() {
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="fixed top-60 left-1/2 transform -translate-x-1/2 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             <span className="text-[hsl(280,100%,70%)]">Notes</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <NotesPage />
+            <NotesPage />
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
             <AuthShowcase />
           </div>
         </div>
@@ -38,25 +33,32 @@ export default function Home() {
 }
 
 function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
+  const { data: session } = useSession();
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
+      {session ? (
+        <div className="fixed right-4 top-4 flex items-center">
+          <img
+            src={session.user.image || "/default-profile.png"}
+            className="mr-2 h-8 w-8 rounded-full"
+          />
+          <span>{session.user.name}</span>
+          <button
+            onClick={() => signOut()}
+            className="ml-4 rounded bg-red-500 px-4 py-2 font-bold text-white"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={() => signIn("google")}
+          className="fixed right-4 top-4 mb-4 rounded bg-blue-500 px-4 py-2 font-bold text-white"
+        >
+          Login com Google
+        </button>
+      )}
     </div>
   );
 }
